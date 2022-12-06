9264779d02863ba1f51e9dfd69a0667305fd2a15
almond-tofu <69336365+almond-tofu@users.noreply.github.com>
2022-12-06 21:50:01 +0800

    Updated preprocessing

--- ML_2.py
+++ ML_2.py
@@ -43,41 +43,49 @@ for column in df:
     for i in range((df_outliers.shape[0])):
         label_index = df_outliers.index[i]
         new_df.loc[label_index, column] = df[column].median()
+display(new_df)
 
 
 #NORMALISATION
 
 from sklearn.preprocessing import MinMaxScaler
 
-def normalize(df):
+#namelist of columns that do not need normalisation for classification
+drop_classif = ['pCR (outcome)', 'ER', 'PgR', 'HER2', 'TrippleNegative', 'ChemoGrade', 'Proliferation', 'HistologyType', 'LNStatus', 'TumourStage']
+
+#namelist of columns that do not need normalisation for regression
+drop_reg = drop_classif + ['RelapseFreeSurvival (outcome)']
+
+def normalize(df, x):
     result = df.copy()
     for feature_name in df.columns:
-        max_value = df[feature_name].max()
-        min_value = df[feature_name].min()
-        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+        if (x == 'classification' and feature_name not in drop_classif) or (x == 'regression' and feature_name not in drop_reg):
+            max_value = df[feature_name].max()
+            min_value = df[feature_name].min()
+            result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
     return result
 
-df_normalisation = normalize(new_df)
+df_classif = normalize(new_df,'classification')
+df_reg = normalize(new_df,'regression')
 
 
 #FEATURE SELECTION
 #Classification 
 
 #separate target variable for classification (PCR)
-x_classif = df_normalisation.drop(labels=["pCR (outcome)"], axis=1)
-y_classif = df_normalisation["pCR (outcome)"]
+x_classif = df_classif.drop(labels=['pCR (outcome)'], axis=1)
+y_classif = df_classif['pCR (outcome)']
 
 #apply ANOVA for classification
 from sklearn.feature_selection import f_classif, SelectKBest
 import math
 
 fs = SelectKBest(score_func = f_classif, k= 53)  #k value for when P_value < 0.05
-
 # Apply feature selection
 fs.fit(x_classif,y_classif)
 
 #declare variable to put selected features for classification
-df_classif = pd.DataFrame()
+df_classif_final = pd.DataFrame()
 
 def list_ceil(x):
     return[math.ceil(i) in x]
@@ -88,31 +96,34 @@ features = pd.DataFrame(x_classif.columns)
 feature_score = pd.concat([features,features_score,features_pvalue],axis=1)
 
 # Assign column names
-feature_score.columns = ["Input_Features","Score","P_Value"]
-chosen_features = feature_score.nlargest(53,columns="Score")
+feature_score.columns = ['Input_Features','Score','P_Value']
+chosen_features = feature_score.nlargest(53,columns='Score')
 
 # Add selected features into a new dataframe
 for feature in chosen_features['Input_Features']:
-    df_classif = pd.concat([df_classif, df_normalisation[feature]], axis=1)
+    df_classif_final = pd.concat([df_classif_final, df_classif[feature]], axis=1)
 
 
 #Regression
 
 #separate target value for regression (RFS)
-x_reg = df_normalisation.drop(labels=["RelapseFreeSurvival (outcome)"], axis=1)
-y_reg = df_normalisation["RelapseFreeSurvival (outcome)"]
+x_reg = df_reg.drop(labels=['RelapseFreeSurvival (outcome)'], axis=1)
+y_reg = df_reg['RelapseFreeSurvival (outcome)']
+
+
+# In[11]:
+
 
 #apply ANOVA for regression
 from sklearn.feature_selection import f_regression, SelectKBest
 import math
 
 fs = SelectKBest(score_func=f_regression,k=20) #k value for when P_value < 0.05
-
 # Apply feature selection
 fs.fit(x_reg,y_reg)
 
 #declare variable to put selected features for classification
-df_reg = pd.DataFrame()
+df_reg_final = pd.DataFrame()
 
 def list_ceil(x):
     return[math.ceil(i) in x]
@@ -123,10 +134,10 @@ features = pd.DataFrame(x_reg.columns)
 feature_score = pd.concat([features,features_score,features_pvalue],axis=1)
 
 # Assign column names
-feature_score.columns = ["Input_Features","Score","P_Value"]
-chosen_features = feature_score.nlargest(20,columns="Score")
+feature_score.columns = ['Input_Features','Score','P_Value']
+chosen_features = feature_score.nlargest(20,columns='Score')
 
 # Add selected features into a new dataframe
 for feature in chosen_features['Input_Features']:
-    df_reg = pd.concat([df_reg, df_normalisation[feature]], axis=1)
+    df_reg_final = pd.concat([df_reg_final, df_reg[feature]], axis=1)
 
