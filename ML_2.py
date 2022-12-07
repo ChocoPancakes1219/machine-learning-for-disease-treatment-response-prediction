380460c433313ef46db1f28b7f2f2019b9d510bd
almond-tofu <69336365+almond-tofu@users.noreply.github.com>
2022-12-07 12:00:19 +0800

    Updated preprocessing method

--- ML_2.py
+++ ML_2.py
@@ -13,20 +13,29 @@ df = pd.read_excel('trainDataset.xls')
 df = df.iloc[:, 1:]
 
 
-#REPLACING MISSING VALUES
+# DELETE MISSING VALUES
 
-#replace missing values (999) of each column to the column's median
+#delete missing values (999) of each column 
 for column in df:
-    df[column]=df[column].replace(999, df[column].median())
+    df[column]=df[column].replace(999, None)
 
-#new_df will be without outliers
-new_df = df
+df = df.dropna()
+
+final_df_classif = df
+final_df_reg = df
+
+#namelist of columns that has categorical data or do not need normalisation for classification
+drop_classif = ['pCR (outcome)', 'ER', 'PgR', 'HER2', 'TrippleNegative', 'ChemoGrade', 'Proliferation', 'HistologyType', 'LNStatus', 'TumourStage']
+
+#namelist of columns that has categorical data or do not need normalisation for regression
+drop_reg = drop_classif + ['RelapseFreeSurvival (outcome)']
 
 #drop the columns with booleans/specific grades as they wont have outliers
-df = df.drop(['pCR (outcome)', 'ER', 'PgR', 'HER2', 'TrippleNegative', 'ChemoGrade', 'Proliferation', 'HistologyType', 'LNStatus', 'TumourStage'], axis=1)
+df_classif = df.drop(drop_classif, axis=1)
+df_reg = df.drop(drop_reg, axis=1)
 
 
-#REPLACING OUTLIERS
+# REPLACING OUTLIERS
 
 #use IQR method to find outliers
 def IQR_outliers(column):
@@ -36,24 +45,23 @@ def IQR_outliers(column):
     df_outliers=df[((df[column]<(Q1-1.5*IQR)) | (df[column]>(Q3+1.5*IQR)))]
     return df_outliers
 
-#replace outliers with median of the column
-for column in df:
-    df_outliers = IQR_outliers(column)
-    
-    for i in range((df_outliers.shape[0])):
-        label_index = df_outliers.index[i]
-        new_df.loc[label_index, column] = df[column].median()
+#delete outliers
+def replace_outliers(df1, df2, drop_list):
+    for column in df1:
+        if column not in drop_list:
+            df_outliers = IQR_outliers(column)
+            for i in range((df_outliers.shape[0])):
+                label_index = df_outliers.index[i]
+                df2.loc[label_index, column] = df2[column].median()
+    return df2
+
+final_df_classif = replace_outliers(df_classif, final_df_classif, drop_classif)
+final_df_reg = replace_outliers(df_reg, final_df_reg, drop_reg)
 
 
-#NORMALISATION
+# NORMALISATION
 
 from sklearn.preprocessing import MinMaxScaler
-
-#namelist of columns that do not need normalisation for classification
-drop_classif = ['pCR (outcome)', 'ER', 'PgR', 'HER2', 'TrippleNegative', 'ChemoGrade', 'Proliferation', 'HistologyType', 'LNStatus', 'TumourStage']
-
-#namelist of columns that do not need normalisation for regression
-drop_reg = drop_classif + ['RelapseFreeSurvival (outcome)']
 
 def normalize(df, x):
     result = df.copy()
@@ -64,22 +72,23 @@ def normalize(df, x):
             result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
     return result
 
-df_classif = normalize(new_df,'classification')
-df_reg = normalize(new_df,'regression')
+normalised_df_classif = normalize(final_df_classif,'classification')
+normalised_df_reg = normalize(final_df_reg,'regression')
 
 
-#FEATURE SELECTION
-#Classification 
+# FEATURE SELECTION
+# Classification 
 
 #separate target variable for classification (PCR)
-x_classif = df_classif.drop(labels=['pCR (outcome)'], axis=1)
-y_classif = df_classif['pCR (outcome)']
+x_classif = normalised_df_classif.drop(labels=['pCR (outcome)'], axis=1)
+y_classif = normalised_df_classif['pCR (outcome)']
 
 #apply ANOVA for classification
 from sklearn.feature_selection import f_classif, SelectKBest
 import math
 
 fs = SelectKBest(score_func = f_classif, k= 53)  #k value for when P_value < 0.05
+
 # Apply feature selection
 fs.fit(x_classif,y_classif)
 
@@ -100,24 +109,21 @@ chosen_features = feature_score.nlargest(53,columns='Score')
 
 # Add selected features into a new dataframe
 for feature in chosen_features['Input_Features']:
-    df_classif_final = pd.concat([df_classif_final, df_classif[feature]], axis=1)
+    df_classif_final = pd.concat([df_classif_final, normalised_df_classif[feature]], axis=1)
 
 
-#Regression
+# Regression
 
 #separate target value for regression (RFS)
-x_reg = df_reg.drop(labels=['RelapseFreeSurvival (outcome)'], axis=1)
-y_reg = df_reg['RelapseFreeSurvival (outcome)']
-
-
-# In[11]:
-
+x_reg = normalised_df_reg.drop(labels=['RelapseFreeSurvival (outcome)'], axis=1)
+y_reg = normalised_df_reg['RelapseFreeSurvival (outcome)']
 
 #apply ANOVA for regression
 from sklearn.feature_selection import f_regression, SelectKBest
 import math
 
 fs = SelectKBest(score_func=f_regression,k=20) #k value for when P_value < 0.05
+
 # Apply feature selection
 fs.fit(x_reg,y_reg)
 
@@ -138,5 +144,5 @@ chosen_features = feature_score.nlargest(20,columns='Score')
 
 # Add selected features into a new dataframe
 for feature in chosen_features['Input_Features']:
-    df_reg_final = pd.concat([df_reg_final, df_reg[feature]], axis=1)
+    df_reg_final = pd.concat([df_reg_final, normalised_df_reg[feature]], axis=1)
 
